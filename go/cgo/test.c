cdcc86a1c29cb9c84ab236cfd312d94a4193d5aa
c <imcling@qq.com>
2022-03-01 16:24:44 +0800

    gcc

--- go/cgo/test.c
+++ go/cgo/test.c
@@ -1,4 +1,4 @@
-#include "test.h"
+#include "include/test.h"
 
 char *test(char *s){
     if(*s == '\0'){
--- go/cgo/test.go
+++ go/cgo/test.go
@@ -1,8 +1,8 @@
 package main
 
 /*
-#cgo CFLAGS: -I./
-#cgo LDFLAGS: -L./ -ltest
+#cgo CFLAGS: -I./include
+#cgo LDFLAGS: -L./lib -ltest -Wl,-rpath,lib
 #include "test.h"
 */
 import "C"
@@ -11,14 +11,14 @@ import "fmt"
 //static
 /*
 gcc -c test.c
-ar -cr libtest.a test.o
+ar -cr ./lib/libtest.a test.o
 go build -o app test.go
 */
 
 //share
 /*
-gcc -shared -fPIC -o libtest.so test.c
-go build -ldflags="-r ./" -o app test.go
+gcc -shared -fPIC -o ./lib/libtest.so test.c
+go build -ldflags="-r ./lib" -o app test.go
 */
 func main() {
 	s1 := C.test(C.CString("golang"))
